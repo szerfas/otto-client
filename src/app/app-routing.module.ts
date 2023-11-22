4f47fcb2d1629d7692029cfe14ed557b555dc4f0
Steve Zerfas <szerfas@yahoo.com>
2023-11-22 15:31:39 -0500

    Clean up default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from '@auth0/auth0-angular';
 import { ManufacturerComponent } from './components/manufacturer/manufacturer.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', title: 'Login Page', component: LoginComponent },
+  { path: '', title: 'Home Page', component: HomeComponent },
   { path: 'home', title: 'Home Page', component: HomeComponent }, 
   { path: 'manufacturer', title: 'Manufacturers', component: ManufacturerComponent, canActivate: [AuthGuard] },
-  { path: '**', title: 'Login Page', component: LoginComponent },
+  { path: '**', title: 'Login Page', component: HomeComponent },
 ];
 
 @NgModule({
